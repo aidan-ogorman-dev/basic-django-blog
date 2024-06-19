370ea5059dcf1f3ef7a9788805baf7b2c52b44de
Aidan O'Gorman <aidanogorman@Aidans-MacBook-Air.local>
2024-06-19 16:08:26 +0100

    Basic blog example using django

--- myproject/views.py
+++ myproject/views.py
@@ -1,8 +1,6 @@
 #from django.http import HttpResponse
 from django.shortcuts import render
 
-# user = aidan
-# pwd = adminuser
 def homepage(request):
     # return HttpResponse("Hello World! I'm home")
     return render(request, 'home.html')
